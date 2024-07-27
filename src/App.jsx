38c26d89e612dd77fc47cd75acf20c7f1eabf6a3
vignesh496 <vigneshg31@icloud.com>
2024-07-28 01:56:27 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './index.css'
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes, useNavigate, useLocation } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
+const FormPage = () => {
+
+  const [name, getName] = useState('');
+  const [email, getEmail] = useState('');
+  const [mobile, getMobile] = useState('');
+  const [suggestions, getSuggestions] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate('/results', {state:
+      {name, email, mobile, suggestions}
+    })
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+       <div className="h-screen bg-[url('assets/sample.png')] bg-cover bg-center bg-no-repeat flex items-center justify-center">
+        <div className='w-full max-w-lg max-h-lg rounded-xl m-10 bg-blue-200 p-4 bg-opacity-75'>
+          <div className='bg-red-400 rounded-md text-center font-bold text-2xl opacity-100'>Sample Form</div>
+          <form onSubmit={handleSubmit} method='GET'>
+          <div className='m-3'>
+            <label htmlFor="name">Name</label>
+            <br/>
+            <input className='transition-transform duration-200 ease-in-out transform hover:scale-110 border-blue-500 rounded-md border-x-2 border-y-2' type="text" value={name} onChange={(e) => getName(e.target.value)}/>
+          </div>
+          <div className='m-3'>
+            <label htmlFor="email">Email</label>
+            <br/>
+            <input className='transition-transform duration-200 ease-in-out transform hover:scale-110 border-blue-500 rounded-md border-x-2 border-y-2' type="email" value={email} onChange={(e) => getEmail(e.target.value)} />
+          </div>
+          <div className='m-3'>
+            <label htmlFor="mobile">Mobile</label>
+            <br/>
+            <input className='transition-transform duration-200 ease-in-out transform hover:scale-110 border-blue-500 rounded-md border-x-2 border-y-2' type="text" value={mobile} onChange={(e) => getMobile(e.target.value)} />
+          </div>
+          <div className='m-3'>
+            <label htmlFor="suggestions">Suggestions</label>
+            <br/>
+            <textarea className='transition-transform duration-200 ease-in-out transform hover:scale-105 border-blue-500 w-full h-32 rounded-md border-x-2 border-y-2' type="text" value={suggestions} onChange={(e) => getSuggestions(e.target.value)} />
+          </div>
+          <div className='m-3'>
+            <button type='submit' className='transition-transform duration-200 ease-in-out transform hover:scale-110 rounded-md bg-green-200 p-1 hover:bg-green-500'>Submit</button>
+          </div>
+          </form>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
+
+
+const ResultPage = () => {
+  const location = useLocation();
+  const { state } = location;
+  return (
+    <div className="h-screen bg-[url('assets/sample.png')] bg-cover bg-center bg-no-repeat flex items-center justify-center">
+      <div className="h-screen flex items-center justify-center">
+        <div className='w-full max-w-lg rounded-xl bg-blue-200 p-4 bg-opacity-75'>
+          <h1 className='text-center font-bold text-2xl'>User Info</h1>
+          <p><strong>Name:</strong> {state.name}</p>
+          <p><strong>Email:</strong> {state.email}</p>
+          <p><strong>Mobile:</strong> {state.mobile}</p>
+          <p><strong>Suggestions:</strong> {state.suggestions}</p>
+        </div>
+      </div>
+    </div>
+    
+  );
+};
+
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<FormPage/>}/>
+        <Route path='/results' element={<ResultPage/>} />
+      </Routes>
+    </Router>
+
+  )
+}
+
 
 export default App
